abe13bb73589a2db8cb2714a582927f158d2bcb6
Houtaroy <Houtaroy@gmail.com>
2021-08-24 09:04:37 +0800

    init repository

---
+++ src/components/Carousel.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>走马灯</div>
+</template>
+<script lang="ts">
+import { defineComponent } from 'vue';
+export default defineComponent({
+  name: 'Carousel'
+});
+</script>
+<style></style>
